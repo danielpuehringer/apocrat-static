2d69d120c2672a35020247f5a95e0b6348e9b492
danielpuehringer11@gmail.com <aegis1112>
2020-11-29 19:07:21 +0100

    WIP Markup of "/" page

--- index.php
+++ index.php
@@ -33,19 +33,74 @@
 
 <body>
 
-    <div class="hero--fullpage">
-        <div class="navbar">
-
+    <div class="hero--fullpage" style="background: url('https://picsum.photos/1920/1080');">
+        <div class="container navbar">
+            <img class="navbar__logo" src="https://picsum.photos/300/45"/>
+            <div class="navbar__list-items">
+                <a class="navbar__link" href="/">Für Privatpersonen</a>
+                <a class="navbar__link" href="/">Für Unternehmen</a>
+                <a class="navbar__link" href="/">Über uns</a>
+                <a class="navbar__link" href="/">Kontakt</a>
+                <a class="navbar__link" href="/">EN</a>
+            </div>
         </div>
-        <div class="hero--fullpage__content">
+        <div class="hero--fullpage__content mx-auto">
             <h1>Schützen, was wichtig ist</h1>
-            <p>
-                Apocrat sichert deine vernetzten Geräte vor unerlaubtem Zugriff wie z.B. einem Hackerangriff.
+            <div class="mb-5">
+                Apocrat sichert deine vernetzten Geräte vor unerlaubtem Zugriff wie z.B. einem Hackerangriff.<br/>
                 Offene Kameras, mithörende Babyphone und manipulierte Türschlösser sind dadurch Geschichte.
+            </div>
+            <div class="font-bold mb-4">Launch: Ende 2021</div>
+            <button class="btn btn-primary border-2 mx-auto" href="/">Mehr erfahren</button>
+        </div>
+    </div>
+    <div class="content-block">
+        <div class="container text-center text-gray-900">
+            <h2 class="text-underline text-underline--center">Schutz für deine IoT-Geräte</h2>
+            <p class="mb-6">
+                Apocrat ist eine AI-basierte Plug-and-Play-"Firewall", die vernetzte Geräte (IoT-Geräte/ Internet of Things Geräte) einerseits davor schützt, von
+                außen übernommen zu werden (z.B. durch Hacker) und andererseits davor bewahrt, dass Nutzerdaten ohne die explizite Zustimmung der User an
+                Dritte weitergegeben werden. Dadurch schützt Apocrat vor Datendiebstahl, dem Eindringen in die Privatsphäre der NutzerInnen und gibt die
+                Höheit über die Nutzerdaten dorthin zurück, wo sie hingehört: zu den einzelnen Menschen.
             </p>
-            <div><span>Launch: Ende 2021</span></div>
-            <a class="btn button-primary" href="">
-        </a>
+            <div class="flex">
+                <div class="w-full md:w-1/3">
+                    <div class="text-yellow font-bold text-3xl md:text-4xl mb-4">270.000</div>
+                    <div class="md:w-56 mx-auto">Angriffe auf vernetzte Geräte pro Tag</div>
+                </div>
+                <div class="w-full md:w-1/3">
+                    <div class="text-yellow font-bold text-3xl md:text-4xl mb-4">+800.000</div>
+                    <div class="md:w-56 mx-auto">Haushalte in Österreich und Deutschland nutzen vernetzte Geräte</div>
+                </div>
+                <div class="w-full md:w-1/3">
+                    <div class="text-yellow font-bold text-3xl md:text-4xl mb-4">&gt;90%</div>
+                    <div class="md:w-56 mx-auto">der vernetzten Geräte kommunizieren unverschlüsselt</div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="content-block bg-grey-400">
+        <div class="container flex">
+            <div class="w-full md:w-1/3">
+                <p>test</p>
+            </div>
+            <div class="w-full md:w-2/3">
+                <h2 class="text-underline">Intelligente Firewall für<br/> dein Heimnetzwerk</h2>
+                <p>
+                    Vernetzte Geräte, die mehr Komfort und Sicherheit in das eigene Heim bringen sind längst zur Selbstverständlichkeit geworden.
+                    Allerdings sind gerade diese Geräte Gefahren wie Hackern of schutzlos ausgeliefert.
+                </p>
+                <p class="font-bold">
+                    Apocrat hilft dir dabei diese Lücke zu schließen und sorgt dafür, dass auch die Geräte, die dir Sicherheit bieten sollen, zukünftig sicher sind.
+                </p>
+                <ul>
+                    <li>Geräte-Erkennung - erkennt Deine Geräte automatisch</li>
+                    <li>Anomalie-Erkennung - unerwünschtes Verhalten wird erkannt und blockiert</li>
+                    <li>Datenschutz - sorgt dafür, dass Deine privaten Daten auch privat bleiben</li>
+                    <li>Smartphone App - zur einfachen Steuerung und Indiviualisierung von Apocrat</li>
+                </ul>
+            </div>
+        </div>
     </div>
 
 </body>
