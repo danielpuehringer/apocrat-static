1bc96aec48bc33010df7ffe8fff304ad0b7b8a10
Daniel <danielpuehringer11@gmail.com>
2020-12-09 22:49:35 +0100

    Finishing first round of page

--- datenschutz.php
+++ datenschutz.php
@@ -23,17 +23,89 @@
 
 <?php include 'includes/headernav.inc' ?>
 
-<div class="container">
-    <h1 class="text-center text-underline text-underline--center">Datenschutzerklärung</h1>
-    <h2 class="text-center mt-4">
+<div class="container mt-16">
+    <h1 class="text-underline text-blue text-md mb-8 text-bold text-lg">Datenschutzerklärung</h1>
+    <h2 class="mt-4 text-base">
         Datenschutz
     </h2>
     <p>
-        Wir haben diese Datenschutzerklärung (Fassung 24.11.2020-211136709) verfasst, um Ihnen gemäß der Vorgaben der <a class="text-blue underline">Datenschutz-Grundverordnung (EU) 2016/679</a> zu erklären, welche Informationen wir sammeln, wie wir Daten verwenden und welche Entscheidungsmöglichkeiten Sie als Besucher dieser Webseite haben.
+        Wir haben diese Datenschutzerklärung (Fassung 24.11.2020-211136709) verfasst, um Ihnen gemäß der Vorgaben der <a href="https://eur-lex.europa.eu/legal-content/DE/ALL/?uri=celex:32016R0679&&tid=211136709" class="text-blue underline">Datenschutz-Grundverordnung (EU) 2016/679</a> zu erklären, welche Informationen wir sammeln, wie wir Daten verwenden und welche Entscheidungsmöglichkeiten Sie als Besucher dieser Webseite haben.
     </p>
     <p>
         Leider liegt es in der Natur der Sache, dass diese Erklärungen sehr technisch klingen, wir haben uns bei der Erstellung jedoch bemüht die wichtigsten Dinge so einfach und klar wie möglich zu beschreiben.
     </p>
+
+    <h2 class="mt-12 text-base">
+        Automatische Datenspeicherung
+    </h2>
+    <p>Wenn Sie heutzutage Webseiten besuchen, werden gewisse Informationen automatisch erstellt und gespeichert, so auch auf dieser Webseite.</p>
+    <p>Wenn Sie unsere Webseite so wie jetzt gerade besuchen, speichert unser Webserver (Computer auf dem diese Webseite gespeichert ist) automatisch Daten wie</p>
+    <ul class="pl-4 list-disc">
+        <li>die Adresse (URL) der aufgerufenen Webseite</li>
+        <li>Browser und Browserversion</li>
+        <li>das verwendete Betriebssystem</li>
+        <li>die Adresse (URL) der zuvor besuchten Seite (Referrer URL)</li>
+        <li>den Hostname und die IP-Adresse des Geräts von welchem aus zugegriffen wird</li>
+        <li>Datum und Uhrzeit</li>
+    </ul>
+    <p>in Dateien (Webserver-Logfiles).</p>
+    <p>In der Regel werden Webserver-Logfiles zwei Wochen gespeichert und danach automatisch gelöscht. Wir geben diese Daten nicht weiter, können jedoch nicht ausschließen, dass diese Daten beim Vorliegen von rechtswidrigem Verhalten eingesehen werden.</p>
+    <h2 class="mt-12 text-base">
+        Speicherung persönlicher Daten
+    </h2>
+    <p>
+        Persönliche Daten, die Sie uns auf dieser Website elektronisch übermitteln, wie zum Beispiel Name, E-Mail-Adresse, Adresse oder andere persönlichen Angaben im Rahmen der Übermittlung eines Formulars oder Kommentaren im Blog, werden von uns gemeinsam mit dem Zeitpunkt und der IP-Adresse nur zum jeweils angegebenen Zweck verwendet, sicher verwahrt und nicht an Dritte weitergegeben.
+    </p>
+    <p>
+        Wir nutzen Ihre persönlichen Daten somit nur für die Kommunikation mit jenen Besuchern, die Kontakt ausdrücklich wünschen und für die Abwicklung der auf dieser Webseite angebotenen Dienstleistungen und Produkte. Wir geben Ihre persönlichen Daten ohne Zustimmung nicht weiter, können jedoch nicht ausschließen, dass diese Daten beim Vorliegen von rechtswidrigem Verhalten eingesehen werden.
+    </p>
+    <p>
+        Wenn Sie uns persönliche Daten per E-Mail schicken – somit abseits dieser Webseite – können wir keine sichere Übertragung und den Schutz Ihrer Daten garantieren. Wir empfehlen Ihnen, vertrauliche Daten niemals unverschlüsselt per E-Mail zu übermitteln.
+    </p>
+
+    <h2 class="mt-12 text-base">
+        Rechte laut Datenschutzgrundverordnung
+    </h2>
+    <p>
+        Ihnen stehen laut den Bestimmungen der DSGVO und des österreichischen <a href="https://www.ris.bka.gv.at/GeltendeFassung.wxe?Abfrage=Bundesnormen&Gesetzesnummer=10001597&tid=211136709" target="_blank">Datenschutzgesetzes (DSG)</a> grundsätzlich die folgende Rechte zu:
+    </p>
+    <ul class="list-disc pl-4">
+        <li>Recht auf Berichtigung (Artikel 16 DSGVO)</li>
+        <li>Recht auf Löschung („Recht auf Vergessenwerden“) (Artikel 17 DSGVO)</li>
+        <li>Recht auf Einschränkung der Verarbeitung (Artikel 18 DSGVO)</li>
+        <li>Recht auf Benachrichtigung – Mitteilungspflicht im Zusammenhang mit der Berichtigung oder Löschung personenbezogener Daten oder der Einschränkung der Verarbeitung (Artikel 19 DSGVO)</li>
+        <li>Recht auf Datenübertragbarkeit (Artikel 20 DSGVO)</li>
+        <li>Widerspruchsrecht (Artikel 21 DSGVO)</li>
+        <li>Recht, nicht einer ausschließlich auf einer automatisierten Verarbeitung — einschließlich Profiling — beruhenden Entscheidung unterworfen zu werden (Artikel 22 DSGVO)</li>
+    </ul>
+    <p>
+        Wenn Sie glauben, dass die Verarbeitung Ihrer Daten gegen das Datenschutzrecht verstößt oder Ihre datenschutzrechtlichen Ansprüche sonst in einer Weise verletzt worden sind, können Sie sich bei der Aufsichtsbehörde beschweren, welche in Österreich die Datenschutzbehörde ist, deren Webseite Sie unter <a href="https://www.dsb.gv.at/" target="_blank">https://www.dsb.gv.at/</a> finden.
+    </p>
+
+    <h2 class="mt-12 text-base">
+        Auswertung des Besucherverhaltens
+    </h2>
+    <p>
+        In der folgenden Datenschutzerklärung informieren wir Sie darüber, ob und wie wir Daten Ihres Besuchs dieser Website auswerten. Die Auswertung der gesammelten Daten erfolgt in der Regel anonym und wir können von Ihrem Verhalten auf dieser Website nicht auf Ihre Person schließen.
+    </p>
+    <p>
+        Mehr über Möglichkeiten dieser Auswertung der Besuchsdaten zu widersprechen erfahren Sie in der folgenden Datenschutzerklärung.
+    </p>
+
+    <h2 class="mt-12 text-base">
+        TLS-Verschlüsselung mit https
+    </h2>
+    <p>
+        Wir verwenden https um Daten abhörsicher im Internet zu übertragen (Datenschutz durch Technikgestaltung <a href="https://eur-lex.europa.eu/legal-content/DE/TXT/HTML/?uri=CELEX:32016R0679&from=DE&tid=211136709" target="_blank">Artikel 25 Absatz 1 DSGVO)</a>. Durch den Einsatz von TLS (Transport Layer Security), einem Verschlüsselungsprotokoll zur sicheren Datenübertragung im Internet können wir den Schutz vertraulicher Daten sicherstellen. Sie erkennen die Benutzung dieser Absicherung der Datenübertragung am kleinen Schlosssymbol links oben im Browser und der Verwendung des Schemas https (anstatt http) als Teil unserer Internetadresse.
+    </p>
+
+
+    <h2 class="mt-12 text-base">
+        Newsletter Datenschutzerklärung
+    </h2>
+    <p>Wenn Sie sich für unseren Newsletter eintragen übermitteln Sie die oben genannten persönlichen Daten und geben uns das Recht Sie per E-Mail zu kontaktieren. Die im Rahmen der Anmeldung zum Newsletter gespeicherten Daten nutzen wir ausschließlich für unseren Newsletter und geben diese nicht weiter.</p>
+    <p>Sollten Sie sich vom Newsletter abmelden – Sie finden den Link dafür in jedem Newsletter ganz unten – dann löschen wir alle Daten die mit der Anmeldung zum Newsletter gespeichert wurden.</p>
+    <p class="mt-4 mb-16">Quelle: Erstellt mit dem Datenschutz Generator von <a href="https://www.firmenwebseiten.at/datenschutz-generator/" target="_blank">firmenwebseiten.at</a> in Kooperation mit <a href="https://www.wallentin.cc/" target="_blank">wallentin.cc</a></p>
 </div>
 
 <body>
--- ueber-uns.php
+++ ueber-uns.php
@@ -22,6 +22,7 @@
 
     <link href="https://www.apocrat.at/ueber-uns" rel="canonical">
 </head>
+<body>
 
 <?php include 'includes/headernav.inc' ?>
 <div class="hero px-2 md:px-0" style="background: url('./img/Bilder_Webseite/processed/marvin-meyer-SYTO3xs06fU-unsplash-(1)_processed.jpg');">
@@ -32,8 +33,96 @@
     </div>
 </div>
 
-<div class="content-block bg-grey-100">
-    <div class="container bg-grey-100">
+<div class="content-block">
+    <div class="container">
+        <h2 class="text-underline text-underline--center text-center">Wonach wir handeln...</h2>
+        <div class="flex flex-col md:flex-row">
+            <div class="w-full md:w-1/3 md:px-2 mt-8 md:mt-0 text-center">
+                <div class="square mx-auto">
+                    <div class="square__image">
+                        <img src="/img/Icons Webseite/fist.png" width="90px" alt="fist"/>
+                    </div>
+                </div>
+                <div class="text-yellow font-bold mt-4 mb-8">
+                    Restore & secure Privacy
+                </div>
+            </div>
+            <div class="w-full md:w-1/3 md:px-2 mt-8 md:mt-0 text-center">
+                <div class="square mx-auto">
+                    <div class="square__image">
+                        <img src="/img/Icons Webseite/shield-with-lock.png" width="90px" alt="lock"/>
+                    </div>
+                </div>
+
+                <div class="text-yellow font-bold mt-4 mb-8">
+                    Power to the People
+                </div>
+            </div>
+            <div class="w-full md:w-1/3 md:px-2 mt-8 md:mt-0 text-center">
+                <div class="square mx-auto">
+                    <div class="square__image" style="background-color: white; width:50px;height:50px;">
+                    </div>
+                </div>
+                <div class="text-yellow font-bold mt-4 mb-8">
+                    Be simple, be transparent
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="content-block bg-gray-100 py-8">
+    <div class="container">
+        <div class="flex flex-col md:flex-row">
+            <div class="w-full md:w-1/2 mx-auto px-4">
+                <h2 class="text-underline">Entstanden im Herzen der europäischen Wissenschaft</h2>
+                <p class="mt-4">
+                    2020 bat sich uns die Gelegenheit, gemeinsam mit Studierenden aus der FH St.Pölten, FH
+                    Krems und der FHWN Wieselburg, als erste Studenten Österreichs an der CERN-BIC Screening
+                    Week teilzunehmen. Das CERN verfolgt damit das Ziel, Studierenden ihre eigens entwickelten
+                    Technologien näher zu bringen, damit diese neue innovative Anwendungsfälle dafür finden
+                    können. Aufgrund unseres Backgrounds im Bereich IT-Security, entschieden wir uns CERN’s
+                    Datenanalyse Framework neues Leben einzuhauchen. Anstatt die Geheimnisse des
+                    Universums zu enthüllen, setzten wir das Framework dafür ein, mehr Sicherheit und
+                    Privatsphäre ins Zuhause zu bringen.
+                </p>
+                <div class="text-yellow font-bold mt-2 mb-6"><a class="hover:underline" href="https://cern-bic.accent.at/bic-screening-weeks/bic-screening-week/" target="_blank" rel="noopener">https://cern-bic.accent.at/bic-screening-weeks/bic-screening-week/</a></div>
+                <div class="w-full" style="height: 336px; background-image: url('img/Bilder_Webseite/processed/cern_first.jpg');background-position: center; background-repeat: no-repeat; background-size: cover"></div>
+            </div>
+
+
+
+            <div class="w-full md:w-1/2 mx-auto px-4">
+                <div class="w-full" style="height: 422px; background-image: url('img/Bilder_Webseite/processed/Praesi_processed.jpg');background-position: center; background-repeat: no-repeat; background-size: cover"></div>
+                <h2 class="text-underline mt-8">Alles hat einen Anfang</h2>
+                <p class="mt-4">
+                    Die CERN Screening Week in Genf endete mit unserem ersten Ideen Pitch für unser Konzept. Aus der damaligen Idee und unter noch anderem Namen (Halberd) entstand unser ausgereiftes Konzept, Apocrat.
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="content-block">
+    <div class="container">
+        <h2 class="text-underline text-underline--center text-center">Unsere Mission</h2>
+        <div class="w-full md:max-w-3xl py-8 mx-auto px-16" style="border: 4px solid #0099CC;">
+            <img src="/img/Icons%20Webseite/king--yellow.png" class="mx-auto my-8" alt="Crown" width="48px" loading="lazy">
+            <div class="text-underline text-underline--center text-center text-blue font-bold text-xl">
+                Die Hoheit über die Daten wieder zurück an den einzelnen Menschen geben.
+            </div>
+            <div class="text-center my-8">
+                <div>Hoheit durch Transparenz</div>
+                <div>Hoheit durch Privacy und Sicherheit</div>
+                <div>Hoheit durch Datenschutz</div>
+                <div>Hoheit durch Entscheidungsfreiheit</div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="content-block bg-gray-100 mb-16 py-8">
+    <div class="container">
         <h2 class="text-underline text-underline--center text-center">Das Team</h2>
         <div class="flex flex-col md:flex-row">
             <div class="w-full md:w-1/4 md:px-2 mt-8 md:mt-0 text-center">
@@ -66,8 +155,6 @@
         </div>
     </div>
 </div>
-
-<body>
 
 <?php include 'includes/footer.inc'; ?>
 
