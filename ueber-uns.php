6624b280a63a5e8eaf5487f49744b6db46f59e55
Daniel <danielpuehringer11@gmail.com>
2020-12-09 08:18:36 +0100

    WIP ueber-uns (added) teams section

--- ueber-uns.php
+++ ueber-uns.php
@@ -22,8 +22,49 @@
 </head>
 
 <?php include 'includes/headernav.inc' ?>
+<div class="hero px-2 md:px-0" style="background: url('./img/Bilder_Webseite/processed/marvin-meyer-SYTO3xs06fU-unsplash-(1)_processed.jpg');">
+        <div class="hero__content">
+            <h1 class="mb-6">Über uns</h1>
+            <div class="font-bold mb-4">Launch: Ende 2021</div>
+        </div>
+    </div>
+</div>
 
-<h1 class="text-center text-underline text-underline--center">Über uns</h1>
+<div class="content-block bg-grey-100">
+    <div class="container bg-grey-100">
+        <h2 class="text-underline text-underline--center text-center">Das Team</h2>
+        <div class="flex flex-col md:flex-row">
+            <div class="w-full md:w-1/4 md:px-2 mt-8 md:mt-0 text-center">
+                <img src="img/Bilder_Webseite/processed/Paul_processed.jpg" loading="lazy" alt="Paul Lackner Porträtaufnahme" />
+                <div class="text-blue-700 font-bold text-lg">Paul Lackner</div>
+                <div class="text-yellow font-bold text-md uppercase">Co-Founder & CEO</div>
+                <a class="block mt-4 font-bold hover:underline" href="mailto:paul.lackner@apocrat.at">Paul.Lackner@apocrat.at</a>
+                <a class="block hover:underline" href="" target="_blank" rel="noopener">LinkedIn(fehlt)</a>
+            </div>
+            <div class="w-full md:w-1/4 md:px-2 mt-8 md:mt-0 text-center">
+                <img src="img/Bilder_Webseite/processed/Armin_processed.jpg" loading="lazy" alt="Armin Huremagic Porträtaufnahme" />
+                <div class="text-blue-700 font-bold text-lg">Armin Huremagic</div>
+                <div class="text-yellow font-bold text-md uppercase">Co-Founder & CTO</div>
+                <a class="block mt-4 font-bold hover:underline" href="mailto:armin.huremagic@apocrat.at">Armin.Huremagic@apocrat.at</a>
+                <a class="block hover:underline" href="" target="_blank" rel="noopener">LinkedIn(fehlt)</a>
+            </div>
+            <div class="w-full md:w-1/4 md:px-2 mt-8 md:mt-0 text-center">
+                <img src="img/Bilder_Webseite/processed/Alex2_processed.jpg" loading="lazy" alt="Alexander Jürgens Porträtaufnahme" />
+                <div class="text-blue-700 font-bold text-lg">Alexander Jürgens</div>
+                <div class="text-yellow font-bold text-md uppercase">Co-Founder & COO</div>
+                <a class="block mt-4 font-bold hover:underline" href="mailto:alexander.juergens@apocrat.at@apocrat.at">Alexander.Juergens@apocrat.at</a>
+                <a class="block hover:underline" href="" target="_blank" rel="noopener">LinkedIn(fehlt)</a>
+            </div>
+            <div class="w-full md:w-1/4 md:px-2 mt-8 md:mt-0 text-center">
+                <img src="img/Bilder_Webseite/processed/Laura_processed.jpg" loading="lazy" alt="Laura Kaltenbrunner Porträtaufnahme" />
+                <div class="text-blue-700 font-bold text-lg">Laura Kaltenbrunner</div>
+                <div class="text-yellow font-bold text-md uppercase">Co-Founder & CMO</div>
+                <a class="block mt-4 font-bold hover:underline" href="mailto:laura.kaltenbrunner@apocrat.at">Laura.Kaltenbrunner@apocrat.at</a>
+                <a class="block hover:underline" href="" target="_blank" rel="noopener">LinkedIn(fehlt)</a>
+            </div>
+        </div>
+    </div>
+</div>
 
 <body>
 
