daba645e2d95b1eb4362ce6d071bcfb3fe0af18a
Daniel <danielpuehringer11@gmail.com>
2020-12-29 16:04:31 +0100

    WIP Privatpersonen

---
+++ privatpersonen.php
@@ -0,0 +1,106 @@
+<!doctype html>
+<html class="no-js" lang="">
+
+<head>
+    <meta charset="utf-8">
+    <title>Über uns | Apocrat</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <meta property="og:title" content="">
+    <meta property="og:type" content="">
+    <meta property="og:url" content="">
+    <meta property="og:image" content="">
+
+    <link rel="manifest" href="site.webmanifest">
+    <link rel="apple-touch-icon" href="icon.png">
+    <meta name="theme-color" content="#fafafa">
+    <!-- Place favicon.ico in the root directory -->
+
+    <link rel="stylesheet" href="/dist/css/main.css">
+    <meta name="description" content="Apocrat für Privatpersonen">
+
+    <link href="https://www.apocrat.at/privatpersonen" rel="canonical">
+</head>
+<body>
+
+<?php include 'includes/headernav.inc' ?>
+<div class="hero px-2 md:px-0" style="background: url('./img/Bilder_Webseite/processed/roberto-nickson-q9nZUFC1nTw-unsplash_processed.jpg');">
+    <div class="hero__content">
+        <h1 class="mb-6">Für Privatpersonen</h1>
+        <div class="font-bold mb-4">Launch: Ende 2021</div>
+    </div>
+</div>
+</div>
+
+<div class="content-block">
+    <div class="container">
+        <h2 class="text-underline text-underline--center text-center">Hol dir zurück, was dir zusteht</h2>
+        <div class="flex flex-col md:flex-row">
+            <div class="w-full md:w-1/3 md:px-2 mt-8 md:mt-0 text-center">
+                <div class="square mx-auto">
+                    <div class="square__image">
+                        <img src="/img/Icons Webseite/king.png" width="90px" alt="king icon"/>
+                    </div>
+                </div>
+                <div class="text-yellow font-bold mt-4 mb-8">
+                    Hoheit über deine Daten
+                </div>
+            </div>
+            <div class="w-full md:w-1/3 md:px-2 mt-8 md:mt-0 text-center">
+                <div class="square mx-auto">
+                    <div class="square__image">
+                        <img src="/img/Icons Webseite/lock.png" width="90px" alt="lock icon"/>
+                    </div>
+                </div>
+
+                <div class="text-yellow font-bold mt-4 mb-8">
+                    Sicherheit & Privatsphäre
+                </div>
+            </div>
+            <div class="w-full md:w-1/3 md:px-2 mt-8 md:mt-0 text-center">
+                <div class="square mx-auto">
+                    <div class="square__image">
+                        <img src="/img/Icons Webseite/silence.png" width="90px" alt="lock icon"/>
+                    </div>
+                </div>
+                <div class="text-yellow font-bold mt-4 mb-8">
+                    Anonymität
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="content-block bg-gray-100 py-8">
+    <div class="container">
+        <h3 class="text-underline text-underline--center text-center">Mit vernetzten Geräten ist Wohnen bequem und einfach - aber auch sicher?</h3>
+        <div class="flex flex-col md:flex-row">
+            <div class="w-full md:w-1/2 mx-auto px-4 mt-8 md:mt-0 flex flex-col align-center justify-center" style="min-height: 256px; background-image: url('img/Bilder_Webseite/processed/nathan-dumlao-Y3AqmbmtLQI-unsplash_processed.jpg');background-position: center; background-repeat: no-repeat; background-size: cover">
+                <div class="text-white font-bold text-xl">Kaputtes Internet der Dinge: <br/>Wenn die Kaffeemaschine Lösegeld fordert</div>
+                <p class="mt-4 text-white">
+                    <small>derStandard.at</small>
+                </p>
+            </div>
+
+            <div class="w-full md:w-1/2 mx-auto px-4">
+                <h3 class="h4 text-underline">270 000 Angriffe pro Tag auf vernetzte Geräte</h3>
+                <p class="mt-4">
+                    Unglaublich viele Angriffe im Internet zielen direkt auf Smart Home und IoT Geräte ab.
+                    Hacker versuchen dabei zum Beispiel Zugriff auf Router oder Webcams zu erhalten um diese Daten zu verkaufen
+                    oder die Opfer auszuspionieren.
+                    Denke daher frühzeitig daran für Sicherheit zu sorgen, um nicht selbst Opfer eines solchen Angriffes zu werden.
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="content-block container py-8">
+
+</div>
+
+
+<?php include 'includes/footer.inc'; ?>
+
+</body>
