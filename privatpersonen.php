53e315945336f597b969102d745526f9d809e411
Daniel <danielpuehringer11@gmail.com>
2020-12-30 16:38:52 +0100

    WIP Privatpersonen

--- privatpersonen.php
+++ privatpersonen.php
@@ -25,7 +25,7 @@
 <body>
 
 <?php include 'includes/headernav.inc' ?>
-<div class="hero px-2 md:px-0" style="background-position: center bottom;background-size: cover;background: url('./img/Bilder_Webseite/processed/roberto-nickson-q9nZUFC1nTw-unsplash_processed.jpg');">
+<div class="hero px-2 md:px-0" style="background-position: center center !important;background-size: cover;background: url('./img/Bilder_Webseite/processed/roberto-nickson-q9nZUFC1nTw-unsplash_processed.jpg');">
     <div class="hero__content">
         <h1 class="mb-6">Für Privatpersonen</h1>
         <div class="font-bold mb-4">Launch: Ende 2021</div>
@@ -76,7 +76,7 @@
     <div class="container">
         <h3 class="text-underline text-underline--center text-center">Mit vernetzten Geräten ist Wohnen bequem und einfach - aber auch sicher?</h3>
         <div class="flex flex-col md:flex-row">
-            <div class="w-full md:w-1/2 mx-auto px-4 mt-8 md:mt-0 flex flex-col justify-center" style="min-height: 256px; background-image: url('img/Bilder_Webseite/processed/nathan-dumlao-Y3AqmbmtLQI-unsplash_processed.jpg');background-position: center; background-repeat: no-repeat; background-size: cover">
+            <div class="w-full md:w-1/2 mx-auto px-4 mt-8 md:mt-0 flex flex-col justify-center" style="min-height: 256px; background-image: url('img/Bilder_Webseite/processed/nathan-dumlao-Y3AqmbmtLQI-unsplash_processed.jpg');background-position: center; background-repeat: no-repeat; background-size: cover; background-color: rgba(0,0,0,0.5);background-blend-mode: multiply;">
                 <div class="text-white font-bold text-xl max-w-lg">Kaputtes Internet der Dinge: <br/>Wenn die Kaffeemaschine Lösegeld fordert</div>
                 <p class="mt-4 text-white">
                     <small>derStandard.at</small>
@@ -119,7 +119,7 @@
 <div class="bg-gray-100 py-8">
     <div class="container">
         <div class="flex flex-col md:flex-row">
-            <div class="w-full md:w-1/2 mx-auto px-4 mt-8 md:mt-0 flex flex-col justify-center" style="min-height: 256px; background-image: url('img/Bilder_Webseite/processed/jonas-leupe-dZmNJKFDuVI-unsplash_processed.jpg');background-position: center; background-repeat: no-repeat; background-size: cover">
+            <div class="w-full md:w-1/2 mx-auto px-4 mt-8 md:mt-0 flex flex-col justify-center" style="min-height: 256px; background-image: url('img/Bilder_Webseite/processed/jonas-leupe-dZmNJKFDuVI-unsplash_processed.jpg');background-position: center; background-repeat: no-repeat; background-size: cover; background-color: rgba(0,0,0,0.5);background-blend-mode: multiply;">
                 <div class="text-white font-bold text-xl max-w-lg">Smart TVs: Fast alle Fernseher spionieren ihre Nutzer aus</div>
                 <p class="mt-4 text-white">
                     <small>derStandard.at</small>
@@ -141,28 +141,98 @@
 
 <div class="container content-block">
     <h3 class="text-underline text-underline--center text-center max-w-lg mx-auto">"Ich habe keine vernetzten Geräte zu Hause" und wieso diese Aussage falsch ist</h3>
-</div>
-
-<div class="content-block bg-blue-300 text-white py-8" style="margin-top: 0;">
-    <div class="container">
-        <h2 class="text-underline text-underline--center text-center text-white">In wenigen Schritten zu Privacy & Datenschutz</h2>
+    <div class="slider">
+        <div class="slide" id="slide-1"></div>
+        <div class="slide" id="slide-2"></div>
+        <div class="slide" id="slide-3"></div>
+        <div class="slide" id="slide-4"></div>
+        <div class="slide" id="slide-5"></div>
     </div>
 </div>
 
-<div class="content-block container py-8">
+<div class="content-block bg-blue-300 text-white py-12 mt-0">
+    <div class="container">
+        <h2 class="text-underline text-underline--center text-center text-white mb-0">In wenigen Schritten zu Privacy & Datenschutz</h2>
+    </div>
+</div>
+
+<div class="content-block container py-8 px-2">
     <div class="flex flex-col md:flex-row">
-        <div class="w-full md:w-25">
+        <div class="w-full md:w-1/4">
             <div class="text-blue font-bold text-lg">Plug & Play Prinzip</div>
             <small class="text-gray-500">Aufsetzen des Geräte</small>
+            <div class="circle__sibling mt-2 md:mt-4"></div>
             <div class="circle__wrapper">
-                <span class="font-bold text-white">01</span>
+                <span class="font-bold text-white mx-auto">01</span>
             </div>
+            <p class="mt-2 md:mt-6 pr-3">
+                Einfach die Apocrat Box per Netzwerkkabel an den Router schließen, den Rest erledigt unsere Software.
+                Im Hintergrund scannt Apocrat das Netzwerk, identifiziert alle IoT-Geräte und zeigt diese über die App an.
+            </p>
+        </div>
+        <div class="w-full md:w-1/4 mt-8 md:mt-0">
+            <div class="text-blue font-bold text-lg">Indviduelle Regeln erstellen</div>
+            <small class="text-gray-500">Dein zu Hause, deine Regeln</small>
+            <div class="circle__sibling mt-2 md:mt-4"></div>
+            <div class="circle__wrapper">
+                <span class="font-bold text-white mx-auto">02</span>
+            </div>
+            <p class="mt-2 md:mt-6 pr-3">
+                Apocrat analysiert die Kommunikation der IoT-Geräte und erstellt automatisiert verhaltensbasierte Regeln.
+                Zusätzlich können über die App auch Nutzer-basierte Regeln erstellt werden.
+            </p>
+        </div>
+        <div class="w-full md:w-1/4 mt-8 md:mt-0">
+            <div class="text-blue font-bold text-lg">AI-basierte Anomalieerkennung</div>
+            <small class="text-gray-500">Künstliche Intelligenz(tbd)</small>
+            <div class="circle__sibling mt-2 md:mt-4"></div>
+            <div class="circle__wrapper">
+                <span class="font-bold text-white mx-auto">03</span>
+            </div>
+            <p class="mt-2 md:mt-6 pr-3">
+                Durch die Erstellung von verhaltensbasierten Regeln durch unseren Machine Learning Algorithmus
+                können Angriffe und Anomalien verlässlicher erkannt werden.
+            </p>
+        </div>
+        <div class="w-full md:w-1/4 mt-8 md:mt-0">
+            <div class="text-blue font-bold text-lg">Verwaltung & Steuerung</div>
+            <small class="text-gray-500">Die Apocrat App</small>
+            <div class="circle__sibling mt-2 md:mt-4"></div>
+            <div class="circle__wrapper">
+                <span class="font-bold text-white mx-auto">04</span>
+            </div>
+            <p class="mt-2 md:mt-6 pr-3">
+                Über die App, die sowohl für iOS als auch Android Geräte zur Verfügung steht, lässt sich die Apocrat
+                Box verwalten und steuern. Einzelne Regeln sowie IoT-Geräte lassen sich dadurch einfach aktivieren oder deaktivieren.
+            </p>
+        </div>
+    </div>
+    <div class="flex flex-col md:flex-row mt-8">
+        <div class="w-full md:w-1/4">
+            <div class="text-blue font-bold text-lg">Stop der Sammelwut</div>
+            <small class="text-gray-500">TBD</small>
+            <div class="circle__sibling mt-2 md:mt-4"></div>
+            <div class="circle__wrapper">
+                <span class="font-bold text-white mx-auto">05</span>
+            </div>
+            <p class="mt-2 md:mt-6 pr-3">
+                Apocrat reduziert die Kommunikation Ihrer IoT-Geräte auf das technische Minimum und gibt
+                Ihnen somit die Entscheidung zurück, welche Daten Sie über Ihr Nutzungsverhalten preisgeben wollen.
+            </p>
+        </div>
+        <div class="w-full md:w-1/4 mt-8 md:mt-0">
+            <div class="text-blue font-bold text-lg">Daten teilen & Vergütung erhalten</div>
+            <small class="text-gray-500">Hoheit über deine Daten</small>
+            <div class="circle__sibling mt-2 md:mt-4"></div>
+            <div class="circle__wrapper">
+                <span class="font-bold text-white mx-auto">06</span>
+            </div>
+            <p class="mt-2 md:mt-6 pr-3">
+                Die Entscheidung, ob Sie nun Ihre eigenen Nutzungsstatistiken teilen, liegt ganz bei Ihnen. Fürs Teilen erhalten Sie
+                eine Reduktion der Lizenzgebühr oder können sich für Spenden an NGOs entscheiden.
+            </p>
         </div>
     </div>
 </div>
-
-
-    <?php include 'includes/footer.inc'; ?>
-
-
+<?php include 'includes/footer.inc'; ?>
 </body>
